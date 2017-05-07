dfc4430d587460e87d8bffd3217686ac1fc9a268
Scott Thiessen <scott@varyable.com>
2017-05-07 13:04:47 -0700

    Add basic react/webpack/typescript build setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path	= require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 module.exports = {
 
@@ -25,6 +28,24 @@ module.exports = {
 			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
 		]
 	},
+
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: "node_modules/react/dist/react.js", to: "vendor/react/" },
+			{ from: "node_modules/react-dom/dist/react-dom.js", to: "vendor/react/" }
+		]),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html'
+		}),
+		new HtmlWebpackIncludeAssetsPlugin({
+			assets: [
+				"vendor/react/react.js",
+				"vendor/react/react-dom.js"
+			],
+			append: false
+		})
+	],
 
 	// When importing a module whose path matches one of the following, just
 	// assume a corresponding global variable exists and use that instead.
