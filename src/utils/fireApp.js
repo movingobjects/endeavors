dce483dbca34f7663ff7189a42d3810e562e3a9b
Scott Thiessen <things@varyd.io>
2017-10-22 12:30:22 -0700

    Add base firebase setup

---
+++ src/utils/fireApp.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyCfceUvLUQrPJTWx-OBN82FtEZe3DKEfNs",
+  authDomain: "endeavorsssss.firebaseapp.com",
+  databaseURL: "https://endeavorsssss.firebaseio.com",
+  projectId: "endeavorsssss",
+  storageBucket: "endeavorsssss.appspot.com",
+  messagingSenderId: "589591316736"
+});
